e7b94c306e6e5967ffa9f2cb2b97f656661c925f
Anna Smolentzov <anna.smolentzov@taxjar.com>
2020-05-15 08:43:26 -0600

    Adds Portfolio edit and update actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -18,7 +18,22 @@ class PortfoliosController < ApplicationController
         format.html { render :new }
       end
     end
+  end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio has been updated' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   private
