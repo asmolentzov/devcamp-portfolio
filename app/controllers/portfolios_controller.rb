1b9f149ada6ed046b6fe2c88447869a09defc9f3
Anna Smolentzov <anna.smolentzov@taxjar.com>
2020-05-15 07:54:13 -0600

    Includes new and create actions for Portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,27 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio is now live' }
+      else
+        format.html { render :new }
+      end
+    end
+
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:blog).permit(:title, :subtitle, :body)
+  end
+
 end
